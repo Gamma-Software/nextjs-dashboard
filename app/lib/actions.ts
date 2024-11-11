0d9b05781deda6c188e0d1c891843c5ee1a3561f
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2024-11-11 16:18:24 +0100

    add actions to add an invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,32 @@
 'use server';
 
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache'; // revalidatePath is used to clear the cache for a specific path
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // or const rawFormData = Object.fromEntries(formData.entries())
-
-  // Test it out:
-  console.log(rawFormData);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
