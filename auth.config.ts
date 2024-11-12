d9921a4ba1a3e06c7bfec1abd2e5b18325805cd3
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2024-11-12 21:56:24 +0100

    remove return

--- auth.config.ts
+++ auth.config.ts
@@ -9,8 +9,6 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      Response.redirect(new URL('/dashboard', nextUrl));
-      return true;
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
